1271ca7ea5387195753e9fa186e6199563736b8c
Carsten Rambow <Carsten.Rambow@medavis.de>
2025-06-04 10:53:54 +0200

    Change default font name and size

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-"use client"
+'use client';
 
-import './App.css'
+import './App.css';
 import { JarsInUseView } from '@/views/jarsInUse/JarsInUseView';
 import { createTheme, CssBaseline, Stack, ThemeProvider } from '@mui/material';
 
 const darkTheme = createTheme({
-  typography: {
-    // In Chinese and Japanese the characters are usually larger,
-    // so a smaller fontsize may be appropriate.
-    fontSize: 12,
-  },
-  palette: {
-    mode: 'dark',
-  },
+    typography: {
+        // In Chinese and Japanese the characters are usually larger,
+        // so a smaller fontsize may be appropriate.
+        fontSize: 11,
+        fontFamily: '"Segoe UI", Arial, sans-serif',
+    },
+    palette: {
+        mode: 'dark'
+    }
 });
 
 function App() {
 
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Stack direction='column' height="100vh" style={{ width: '100%' }}>
-        <JarsInUseView />
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Stack direction="column" height="100vh" style={{ width: '100%' }}>
+                <JarsInUseView />
 
-        { /* Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src='./node.svg' alt='Node logo' />
+                { /* Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src='./node.svg' alt='Node logo' />
         <UpdateElectron />
         */}
-      </Stack>
-    </ThemeProvider>
-  )
+            </Stack>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
