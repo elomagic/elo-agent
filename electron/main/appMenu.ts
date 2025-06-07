5f3a20457f38cf68d33ab6dffb3b2ac59492044f
C. Rambow <rambow.dev@elomagic.de>
2025-06-07 06:26:33 +0200

    Fix

--- electron/main/appMenu.ts
+++ electron/main/appMenu.ts
@@ -1,39 +1,21 @@
-import {Menu, shell} from 'electron';
+import {Menu} from 'electron';
 import MenuItemConstructorOptions = Electron.MenuItemConstructorOptions;
 import MenuItem = Electron.MenuItem;
 
-export const initAppMenu = () => {
+export const createAppMenu = () => {
 
     const template: Array<(MenuItemConstructorOptions) | (MenuItem)> = [
         {
             label: 'elo Agent',
             submenu: [
                 {
-                    label: 'About',
-                },
-                {
-                    type: 'separator'
-                },
-                {
                     label: 'Quit',
                     role: 'quit',
                     toolTip: 'Quit this application'
                 }
             ]
-        },
-        {
-            label: 'Help',
-            submenu: [
-                {
-                    label: 'Search issues',
-                    click() {
-                        shell.openExternal("https://github.com/elomagic/jr-agent-app/issues")
-                    }
-                },
-            ]
         }
     ]
 
-    const menu = Menu.buildFromTemplate(template)
-    Menu.setApplicationMenu(menu)
+    return Menu.buildFromTemplate(template)
 }
